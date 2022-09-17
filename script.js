d03cfc6f7dffb12a337dee15d136fd66bc664deb
Suzuki Jhordan <jhordan.kendy@outlook.com>
2022-09-17 15:02:07 -0300

    add list questions

---
+++ script.js
@@ -0,0 +1,62 @@
+const titleQuestion = document.querySelector('h2');
+const allAlternates = document.querySelectorAll('h4');
+const back = document.querySelector('.anterior');
+const next = document.querySelector('.proxima');
+
+const questions = [{
+        title: 'Quais as ordens das faixas do Jiu-Jitsu?',
+        a: 'Roxa, Azul, Branca, Cinza, Laranja, Amarela, Verde, Preta, Marrom, Vermelha e Coral.',
+        b: 'Azul, Roxa, Branca, Laranja, Verde, Marrom, Cinza, Vermelha, Preta, Coral e Branca.',
+        c: 'Branca, Cinza, Amarela, Laranja, Verde, Azul, Roxa, Marrom, Preta, Coral e Vermelha.',
+        d: 'Cinza, Preta, Marron, Branca, Coral, Vermelha, Azul, Roxa, Cinza, Amarela e Verde.'
+    },
+    {
+        title: 'Qual dos golpes abaixo NÃO é permitido na faixa Branca?',
+        a: 'Arm-lock',
+        b: 'Triangulo',
+        c: 'Mão de vaca',
+        d: 'Omoplata'
+    },
+    {
+        title: 'O Jiu-Jitsu é uma arte muito utilizada no MMA. Na luta entre Anderson Silva e Chael Sonnem, pelo UFC, o brasileiro acertou um golpe que "apagou" o americano. Que golpe foi esse ?',
+        a: 'Mata-Leão',
+        b: 'Arm-Lock',
+        c: 'Triangulo',
+        d: 'omoplata'
+    },
+    {
+        title: 'Um mestre faixa preta pode graduar seus alunos até qual faixa?',
+        a: 'Marrom',
+        b: 'Roxa',
+        c: 'Verde',
+        d: 'Preta'
+    },
+    {
+        title: 'Qual o Nome do estilo de jiu jitsu cuja luta é feita sem kimono?',
+        a: 'Submission',
+        b: 'SubJitsu',
+        c: 'Jiu-Jitsu Sub',
+        d: 'Absoluto'
+    },
+    {
+        title: 'Qual o Nome da família que deu ao jiu jitsu a adaptação ao conhecido Brazilian Jiu Jitsu?',
+        a: 'Família Machado',
+        b: 'Família Nogueira',
+        c: 'Família Souza',
+        d: 'Família Gracie'
+    },
+    {
+        title: 'Qual a idade mínima para um praticante de jiu jitsu se tornar faixa preta?',
+        a: '16',
+        b: '18',
+        c: '19',
+        d: '20'
+    },
+    {
+        title: 'Em que ano o Jiu Jitsu chegou ao Brasil?',
+        a: '1915',
+        b: '1964',
+        c: '1914',
+        d: '1985'
+    },
+];
